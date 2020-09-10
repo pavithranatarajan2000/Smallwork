a540836e2de995c3c363a9d4df361f6fd7e3c565
pavithranatarajan2000 <58357646+pavithranatarajan2000@users.noreply.github.com>
2020-09-10 11:13:51 +0530

    Update emoji.py

--- emoji_files/emoji.py
+++ emoji_files/emoji.py
@@ -1,3 +1,10 @@
 import emoji
 print(emoji.emojize(':thumbs_up:')
 print(emoji.emojize(':cookies:')
+      
+      
+      
+  
+      '''link for unicode or emoji keyword:
+      https://unicode.org/emoji/charts/emoji-list.html
+      '''
