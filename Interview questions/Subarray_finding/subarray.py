8c9f95a6d9d314630a15ccf63a70c99644932408
pavithranatarajan2000 <58357646+pavithranatarajan2000@users.noreply.github.com>
2020-10-12 12:09:46 +0530

    Update subarray.py

--- Interview questions/Subarray_finding/subarray.py
+++ Interview questions/Subarray_finding/subarray.py
@@ -1,1 +1,8 @@
 #Python program
+
+import numpy as np
+n=int(input())
+m=int(input())
+A=np.array(list(map(int,input().split()[:n])))
+B=np.array(list(map(int,input().split()[:m])))
+print((np.intersect1d(A,B)).size)
